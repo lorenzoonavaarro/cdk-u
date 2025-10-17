3e903d01e124d8ca2fc0cdfe95b1b5f35c4e5d77
Lorenzo Navarro <lorenzoonavaarro@proton.me>
2025-10-17 17:14:33 -0500

    (feat) 3-level constructs added

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -1,16 +1,41 @@
 import * as cdk from 'aws-cdk-lib';
+import { Bucket, CfnBucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+class L3Bucket extends Construct{
+  constructor(scope: Construct, id: string, expiration: number){
+    super(scope, id);
+    
+    new Bucket(this, 'L3Bucket', {
+      lifecycleRules: [{
+        expiration: cdk.Duration.days(expiration)
+      }]
+    });
+  }
+}
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // L1 Construct
+    new CfnBucket(this, 'MyL1Bucket', {
+      lifecycleConfiguration: {
+        rules: [{
+          expirationInDays: 1,
+          status: 'Enabled'
+        }]
+      }
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // L2 Construct
+    new Bucket(this, 'MyL2Bucket', {
+      lifecycleRules: [{
+        expiration: cdk.Duration.days(2)
+      }]
+    });
+
+    // Calling L3 Construct
+    new L3Bucket(this, 'L3Bucket', 3);
   }
 }
