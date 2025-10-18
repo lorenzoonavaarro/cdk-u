48487060bf7ffbe4518947f19335ee0f3f6150b3
Lorenzo Navarro <lorenzoonavaarro@proton.me>
2025-10-17 20:59:34 -0500

    (fix) bucket name generator

--- lib/PhotoStack.ts
+++ lib/PhotoStack.ts
@@ -1,21 +1,26 @@
-import { CfnOutput, Stack, StackProps } from "aws-cdk-lib";
+import { CfnOutput, Fn, Stack, StackProps } from "aws-cdk-lib";
 import { Bucket, CfnBucket } from "aws-cdk-lib/aws-s3";
 import { Construct } from "constructs";
 
 export class PhotoStack extends Stack{
+    private stackSuffix: string;
+
     constructor(scope: Construct, id: string, props?: StackProps){
         super(scope, id, props);
+        
+        this.initializeSuffix();
 
         const myBucket = new Bucket(this, 'PhotoBucket2', {
-            bucketName: 'photosbucket-234ks1'
+            bucketName: `photos-bucket-${this.stackSuffix}`
         });
 
         new CfnOutput(this, 'BucketName', {
             value: myBucket.bucketName
-        });
+        })
+    }
 
-        // reference to change the logical ID value
-        (myBucket.node.defaultChild as CfnBucket).overrideLogicalId('PhotosBucket');
-
+    private initializeSuffix() {
+        const shortStackId = Fn.select(2, Fn.split('/', this.stackId));
+        this.stackSuffix = Fn.select(4, Fn.split('-', shortStackId));
     }
 }
